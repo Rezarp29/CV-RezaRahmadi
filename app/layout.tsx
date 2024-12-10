cc69ee601a881b940c3cebb0933eb9e547e6e16a
Rezarp29 <109502889+Rezarp29@users.noreply.github.com>
2024-12-10 07:41:09 +0700

    new

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,21 @@
 // app/layout.tsx
 "use client";
-import { ThemeProvider } from './contexts/ThemeContext';
 import './globals.css';
+import { ThemeProvider } from './contexts/ThemeContext';
+import Layout from './components/Layout';
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body>
         <ThemeProvider>
-          {children}
+          <Layout>
+            {children}
+          </Layout>
         </ThemeProvider>
       </body>
     </html>
